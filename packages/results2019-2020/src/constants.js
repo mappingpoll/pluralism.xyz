5a3d50e51466a0888d414a870a8c70bc80813652
Nicolas Epstein <nilueps@gmail.com>
2021-12-05 18:45:55 -0500

    initial commit

---
+++ packages/results2019-2020/src/constants.js
@@ -0,0 +1,138 @@
+import { interpolateRgbBasis } from "d3";
+
+export const NA_SYMBOL = "NA";
+
+export const GRAPH_TYPE = {
+  scatterplot: "scatterplot",
+  heatmap: "heatmap",
+  density: "density scatterplot",
+  contour: "contour",
+  colorContour: "color contour",
+  contourScatterplot: "contour scatterplot",
+  numbers: "numbers",
+  proportions: "proportions",
+};
+export const COLOR_SCHEME = {
+  greyscale: "interpolateGreys",
+  viridis: "interpolateViridis",
+  plasma: "interpolatePlasma",
+  warm: "interpolateWarm",
+  cividis: "interpolateCividis",
+  coolwarm: "coolwarm",
+  nicolas: "nicolas",
+  nicolas2: "nicolas2",
+  nicolas3: "nicolas3",
+  nicolas4: "nicolas4",
+};
+
+export const CUSTOM_COLORS = {
+  coolwarm: interpolateRgbBasis(["CornflowerBlue", "DimGray", "IndianRed"]),
+  nicolas: interpolateRgbBasis([
+    "#de0000",
+    "#b9001e",
+    "#a5013c",
+    "#920e64",
+    "#782793",
+    "#694dc6",
+    "#4e6ee3",
+    "#1e83ff",
+    "#03a2ff",
+  ]),
+  nicolas2: interpolateRgbBasis([
+    "#efddff",
+    "#e6abe6",
+    "#d65da4",
+    "#d65da4",
+    "#640700",
+  ]),
+  nicolas3: interpolateRgbBasis(["#eeeeee", "#272727"]),
+  nicolas4: interpolateRgbBasis([
+    "#e2e2e2",
+    "#c4c4c4",
+    "#a1a1a1",
+    "#7c7c7c",
+    "#5f5f5f",
+    "#4a4a4a",
+    "#3b3b3b",
+    "#2d2d2d",
+  ]),
+};
+export const DEFAULT_DOT_COLOR = "black";
+export const DEFAULT_COLOR_SCHEME = COLOR_SCHEME.nicolas4;
+export const DEFAULT_GRAPH_TYPE = GRAPH_TYPE.scatterplot;
+export const DEFAULT_DOT_SIZE = 22;
+export const DEFAULT_DOT_OPACITY = 0.28;
+export const HIGHLIGHT_OPACITY = 1;
+export const HIGHLIGHT_COLOR = "red";
+export const DEFAULT_COLOR_MID = 1;
+export const DEFAULT_CANVAS_WIDTH = 1000;
+export const DEFAULT_CANVAS_HEIGHT = 1000;
+export const DEFAULT_CANVAS_MARGIN = {
+  top: 30,
+  bottom: 30,
+  right: 100,
+  left: 100,
+};
+
+// should be same as --track-width, --track-height, etc in src/style/index.css
+export const TRACK_WIDTH = 400;
+export const TRACK_HEIGHT = 5;
+export const THUMB_HEIGHT = 20;
+export const THUMB_WIDTH = 20;
+
+export const DATASETS = {
+  language: ["en", "fr"],
+  form: ["aga", "ba"],
+};
+export const MARGIN = DEFAULT_CANVAS_MARGIN;
+export const UNCERTAINTY = 0.5;
+export const DOMAIN = [-15, 15];
+export const AXES_DOMAIN = [-10, 10];
+function rangeDiscreet(range) {
+  const min = Math.min(...range),
+    max = Math.max(...range);
+  return new Array(max - min + 1).fill(0).map((_, i) => i + min);
+}
+export const DOMAIN_DISCREET = rangeDiscreet(DOMAIN);
+export const AXES_DOMAIN_DISCREET = rangeDiscreet(AXES_DOMAIN);
+export const ORIGIN = {
+  x: MARGIN.left + DEFAULT_CANVAS_WIDTH / 2,
+  y: MARGIN.top + DEFAULT_CANVAS_HEIGHT / 2,
+};
+export const VIEWBOX = [
+  0,
+  0,
+  MARGIN.left + DEFAULT_CANVAS_WIDTH + MARGIN.right,
+  MARGIN.top + DEFAULT_CANVAS_HEIGHT + MARGIN.bottom,
+];
+
+export const INITIAL_STATE = {
+  data: null,
+  filteredDataset: null,
+  standardRegionCounts: null,
+  questions: null,
+  vizColumns: [],
+  standardColumnSet: [],
+  customViz: false,
+  userAxes: {
+    x: "",
+    y: "",
+  },
+  brushMap: {},
+  colorScale: () => {}, //getColorScale(DEFAULT_COLOR_SCHEME, AXES_DOMAIN),
+  options: {
+    reverseColor: false,
+    size: DEFAULT_DOT_SIZE,
+    opacity: DEFAULT_DOT_OPACITY,
+    contourBandwidth: 25,
+    graph: DEFAULT_GRAPH_TYPE,
+    color: DEFAULT_COLOR_SCHEME,
+    k: DEFAULT_COLOR_MID,
+    dataset: {
+      aga: true,
+      ba: true,
+      en: true,
+      fr: true,
+    },
+  },
+};
---
+++ packages/results2019-2020/src/lib/data-manipulation.js
@@ -0,0 +1,188 @@
+import { cloneDeep } from "lodash";
+import { UNCERTAINTY } from "../constants";
+import jitter from "../fetch/jitter";
+import { isValidDatum } from "./viztools";
+
+export function cleanQuestions(data, header) {
+  return Object.keys(data[0]).filter(
+    q => q != "poll" && q != "Language" && q != "id"
+  );
+}
+
+export function getPairwiseColumns(questions) {
+  let pairs = [];
+  // iterate pairwise
+  for (let idx = 0; idx < questions.length; idx += 2) {
+    const columns = [questions[idx], questions[idx + 1]];
+    pairs.push(columns);
+  }
+  return pairs;
+}
+
+export function getCustomColumns(questions, axes) {
+  return Object.values(axes)
+    .filter(a => a !== "")
+    .map(a => questions[a]);
+}
+
+export function filterDataByDataset(data, dataset) {
+  return data.filter(d => {
+    for (let condition in dataset) {
+      if (d.Language === condition && !dataset[condition]) return false;
+      if (d.poll.toLowerCase() === condition && !dataset[condition])
+        return false;
+    }
+    return true;
+  });
+}
+
+export function filterDataByRange(data, column, range) {
+  return data.filter(
+    d =>
+      isValidDatum(d, column) && range[0] <= d[column] && d[column] <= range[1]
+  );
+}
+
+export function inRange(n, range) {
+  if (isNaN(n)) return false;
+  return range[0] <= n && n <= range[1];
+}
+
+export function inStandardColumnSet(columnSet, columns) {
+  return (
+    columnSet.find(pair => pair[0] === columns[0] && pair[1] === columns[1]) !=
+    null
+  );
+}
+
+export function applyJitter(data) {
+  const dataCopy = cloneDeep(data);
+  for (const row of dataCopy) {
+    for (const key in row) {
+      if (typeof row[key] === "number") row[key] += jitter(UNCERTAINTY);
+    }
+  }
+  return dataCopy;
+}
+
+function sumRegionCounts(region) {
+  const sumProps = obj =>
+    Object.keys(obj).reduce((sum, key) => sum + obj[key], 0);
+
+  return (
+    region.origin +
+    sumProps(region.axes) +
+    sumProps(region.outerAxes) +
+    sumProps(region.quadrants) +
+    sumProps(region.outerQuadrants)
+  );
+}
+
+function roundProps(obj) {
+  for (let key in obj) {
+    obj[key] = Math.round(obj[key]);
+  }
+  return obj;
+}
+
+export function countGraphRegionProportions(data, columns, count = null) {
+  if (count == null) count = countGraphRegions(data, columns);
+  const total = sumRegionCounts(count);
+  const proportions = {
+    origin: (count.origin / total) * 100,
+    quadrants: {
+      nw: ((count.quadrants.nw + count.outerQuadrants.nw) / total) * 100,
+      ne: ((count.quadrants.ne + count.outerQuadrants.ne) / total) * 100,
+      se: ((count.quadrants.se + count.outerQuadrants.se) / total) * 100,
+      sw: ((count.quadrants.sw + count.outerQuadrants.sw) / total) * 100,
+    },
+    axes: {
+      n: ((count.axes.n + count.outerAxes.n) / total) * 100,
+      e: ((count.axes.e + count.outerAxes.e) / total) * 100,
+      s: ((count.axes.s + count.outerAxes.s) / total) * 100,
+      w: ((count.axes.w + count.outerAxes.w) / total) * 100,
+    },
+  };
+  proportions.origin = Math.round(proportions.origin);
+  proportions.quadrants = roundProps(proportions.quadrants);
+  proportions.axes = roundProps(proportions.axes);
+  return proportions;
+}
+
+export function countGraphRegions(data, columns) {
+  const [a, b] = columns;
+  const region = {
+    origin: 0,
+    quadrants: { nw: 0, ne: 0, se: 0, sw: 0 },
+    outerQuadrants: { nw: 0, ne: 0, se: 0, sw: 0 },
+    axes: { n: 0, e: 0, s: 0, w: 0 },
+    outerAxes: { n: 0, e: 0, s: 0, w: 0 },
+  };
+  data.forEach(respondent => {
+    let x = respondent[a],
+      y = respondent[b];
+    if (typeof x === "number" && typeof y === "number") {
+      // origin
+      if (x === 0 && y === 0) {
+        region.origin++;
+        // quadrants
+      } else if ([x, y].every(n => n >= -10 && n <= 10 && n !== 0)) {
+        if (x < 0 && y < 0) {
+          region.quadrants.sw++;
+        } else if (x > 0 && y < 0) {
+          region.quadrants.se++;
+        } else if (x > 0 && y > 0) {
+          region.quadrants.ne++;
+        } else if (x < 0 && y > 0) {
+          region.quadrants.nw++;
+        }
+        // axes
+      } else if (x === 0) {
+        if (y < -10) {
+          region.outerAxes.s++;
+        } else if (y > 10) {
+          region.outerAxes.n++;
+        } else if (y < 0) {
+          region.axes.s++;
+        } else if (y > 0) {
+          region.axes.n++;
+        }
+      } else if (y === 0) {
+        if (x < -10) {
+          region.outerAxes.w++;
+        } else if (x > 10) {
+          region.outerAxes.e++;
+        } else if (x < 0) {
+          region.axes.w++;
+        } else if (x > 0) {
+          region.axes.e++;
+        }
+        //outer quadrants
+      } else if ([x, y].every(n => n < -10 || n > 10)) {
+        if (x < -10 && y < -10) {
+          region.outerQuadrants.sw++;
+        } else if (x > 10 && y < -10) {
+          region.outerQuadrants.se++;
+        } else if (x > 10 && y > 10) {
+          region.outerQuadrants.ne++;
+        } else if (x < -10 && y > 10) {
+          region.outerQuadrants.nw++;
+        }
+      }
+    }
+  });
+  return region;
+}
+
+export function countStandardSetGraphRegions(data, header) {
+  const counts = {};
+  // pairwise iteration
+  for (let i = 0; i < header.length; i += 2) {
+    const a = header[i],
+      b = header[i + 1];
+
+    // save region counts under both colnames for easy reference later
+    counts[a] = counts[b] = countGraphRegions(data, [a, b]);
+  }
+  return counts;
+}
