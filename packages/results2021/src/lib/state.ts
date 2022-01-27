651375eed20fdb69dc1995d4aa997fdd2e19c4b7
Nicolas Epstein <nilueps@gmail.com>
2022-01-26 21:29:08 -0500

    state

--- packages/results2021/src/lib/state.ts
+++ packages/results2021/src/lib/state.ts
@@ -1,23 +1,14 @@
-import { BASE_OPACITY, OPACITY_K } from "./constants";
 import { Pair } from "./questions";
 import { graphs } from "../components/graphs";
 
 export interface State {
   selectedUsers: string[];
   customPair: Pair;
-  opacity: {
-    base: number;
-    k: number;
-  };
   graph: keyof typeof graphs;
 }
 
 export const initialState: State = {
   selectedUsers: [],
-  customPair: { x: "21", y: "21" },
-  opacity: {
-    base: BASE_OPACITY,
-    k: OPACITY_K,
-  },
-  graph: "Boxes",
+  customPair: { x: "", y: "" },
+  graph: "Rectangles",
 };
