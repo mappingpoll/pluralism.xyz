07beb63972f390019ea9852f2d7627ea9292aa9a
Nicolas Epstein <nilueps@gmail.com>
2022-05-28 19:48:01 -0400

    tic

--- frontend/build.js
+++ frontend/build.js
@@ -2,6 +2,8 @@ const esbuild = require("esbuild");
 const { copyFile, readdir, cp, mkdir, rm } = require("fs/promises");
 const path = require("path");
 
+const { pnpPlugin } = require("@yarnpkg/esbuild-plugin-pnp");
+
 const BUILD_DIR = "build";
 
 const preserveAbsImgPath = {
@@ -29,14 +31,14 @@ const images = async () => {
 
 const assets = async () => {
   await cp("assets", `${BUILD_DIR}/assets`, { recursive: true });
-}
+};
 
 const buildConfig = {
-  plugins: [preserveAbsImgPath],
+  plugins: [preserveAbsImgPath, pnpPlugin()],
   bundle: true,
   format: "esm",
   target: ["es6", "edge95", "firefox93", "chrome92", "safari14"],
-  sourcemap: isDev ? 'inline' : false,
+  sourcemap: isDev ? "inline" : false,
   minify: !isDev,
   watch: isDev,
   logLevel: "info",
@@ -50,7 +52,7 @@ async function copyDirContent(dir, outdir) {
 }
 
 const homepage = async () => {
-  await copyDirContent("packages/homepage", BUILD_DIR);
+  await copyDirContent("frontend/packages/homepage", BUILD_DIR);
 };
 
 const results20192020 = async () => {
@@ -58,11 +60,11 @@ const results20192020 = async () => {
   const OUTDIR = `${BUILD_DIR}/${APP_NAME}`;
 
   await mkdir(OUTDIR);
-  await copyFile("packages/results2019-2020/src/index.html", path.join(OUTDIR, "index.html"));
-  await cp("packages/results2019-2020/src/assets", path.join(OUTDIR, "assets"), { recursive: true });
+  await copyFile("frontend/packages/results2019-2020/src/index.html", path.join(OUTDIR, "index.html"));
+  await cp("frontend/packages/results2019-2020/src/assets", path.join(OUTDIR, "assets"), { recursive: true });
   await esbuild.build({
     ...buildConfig,
-    entryPoints: ["packages/results2019-2020/src/index.jsx"],
+    entryPoints: ["frontend/packages/results2019-2020/src/index.jsx"],
     publicPath: `https://www.pluralism.xyz/${APP_NAME}`,
     jsxFactory: "h",
     jsxFragment: "Fragment",
@@ -76,7 +78,7 @@ const results20192020 = async () => {
 
 const results2021 = async () => {
   const APP_NAME = "survey2021";
-  const DIR = "packages/results2021/src";
+  const DIR = "frontend/packages/results2021/src";
   const OUTDIR = `${BUILD_DIR}/${APP_NAME}`;
 
   await mkdir(OUTDIR);
