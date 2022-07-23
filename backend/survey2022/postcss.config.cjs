c35e8df1c127f830b215a57cc74fb28ae7ec677b
Nicolas Epstein <nilueps@gmail.com>
2022-07-23 15:44:46 -0400

    optimize: webp, prefetch, minify css

--- backend/survey2022/postcss.config.cjs
+++ backend/survey2022/postcss.config.cjs
@@ -1,19 +1,24 @@
 module.exports = {
-	plugins: [
-		require('postcss-import')({
-			plugins: [require('stylelint')({
-				fix: true
-			})]
-		}),
-		require('postcss-media-minmax')(),
-		require('postcss-preset-env')({
-			stage: 3,
-			features: {
-				'nesting-rules': true
-			}
-		}),
-		require('stylelint')({
-			fix: true
-		})
-	]
+  plugins: [
+    require("postcss-import")({
+      plugins: [
+        require("stylelint")({
+          fix: true,
+        }),
+      ],
+    }),
+    require("postcss-media-minmax")(),
+    require("postcss-preset-env")({
+      stage: 3,
+      features: {
+        "nesting-rules": true,
+      },
+    }),
+    require("stylelint")({
+      fix: true,
+    }),
+    require("cssnano")({
+      preset: "default",
+    }),
+  ],
 };
